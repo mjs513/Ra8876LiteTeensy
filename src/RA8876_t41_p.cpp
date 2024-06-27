28802d841ada43eb278afba62c209b58d802a0d5
Mike S <CyberMerln@gmail.com>
2024-06-27 06:55:52 -0400

    Revert delayNano change

--- src/RA8876_t41_p.cpp
+++ src/RA8876_t41_p.cpp
@@ -3413,7 +3413,7 @@ ru16 des_x,ru16 des_y,ru16 width,ru16 height,ru8 rop_code,const unsigned char *d
   DCHigh();
   for(j=0;j<height;j++) {
 	for(i=0;i<width;i++) {
-  delayNanoseconds(10);  // Initially setup for the T4.1 board
+  //delayNanoseconds(10);  // Initially setup for the T4.1 board
       if(_rotation & 1) delayNanoseconds(20);
       p->SHIFTBUF[0] = *data++;
       // Wait for transfer to be completed
